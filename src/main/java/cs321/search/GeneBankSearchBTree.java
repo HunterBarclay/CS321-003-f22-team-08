8b770406f4abba848e7e3a0dcd4165b6366b47a6
Hunter <hunter.barclay18@gmail.com>
2023-04-28 21:49:44 -0600

    Potential search fix

--- src/main/java/cs321/search/GeneBankSearchBTree.java
+++ src/main/java/cs321/search/GeneBankSearchBTree.java
@@ -46,22 +46,17 @@ public class GeneBankSearchBTree
                 treeObj = loadedTree.search(subsequenceQueryLong);
             }
 
-            if (treeObj != null) {
-                String subsequenceComplimentString = SequenceUtils.getComplement(subsequenceQueryString);
-                long subsequenceComplimentLong = SequenceUtils.dnaStringToLong(subsequenceComplimentString);
+            String subsequenceComplimentString = SequenceUtils.getComplement(subsequenceQueryString);
+            long subsequenceComplimentLong = SequenceUtils.dnaStringToLong(subsequenceComplimentString);
 
-                TreeObject<Long> treeObj2;
-                if (geneBankSearchBTreeArguments.getUseCache()) {
-                    treeObj2 = loadedTree.search(subsequenceComplimentLong, cache);
-                } else {
-                    treeObj2 = loadedTree.search(subsequenceComplimentLong);
-                }
-
-                numInstances = treeObj.getInstances();
-                if (treeObj2 != null) {
-                    numInstances += treeObj2.getInstances();
-                }
+            TreeObject<Long> treeObj2;
+            if (geneBankSearchBTreeArguments.getUseCache()) {
+                treeObj2 = loadedTree.search(subsequenceComplimentLong, cache);
+            } else {
+                treeObj2 = loadedTree.search(subsequenceComplimentLong);
             }
+
+            numInstances = (treeObj == null ? 0 : treeObj.getInstances()) + (treeObj2 == null ? 0 : treeObj2.getInstances());
             
             System.out.println(subsequenceQueryString + " " + numInstances);
 
